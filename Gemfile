0f74e7366a631012e0db81af2f627263304b4a5d
Chris Bolton <saiko_chriskun@shortmail.me>
2012-03-26 18:36:41 -0700

    Initial upload.

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'puma'
+gem 'sorcery'
+gem 'mongoid'
+gem 'bson_ext'
+gem 'decent_exposure'
+gem 'inherited_resources'
+gem 'haml'
+gem 'juggernaut'
+gem 'stalker'
+gem 'pry'
+gem 'pony'
+gem 'foreman'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '97854480659483ba1f8cb23548e15e2578debd52dbd1dd47557a915362bfd62d993916b6ed18f83cd726717622f1438eacfb642822c1a02d10d613482f18a38a'
