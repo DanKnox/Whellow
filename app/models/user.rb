a5975d138555fc8c222e966d1f535c46b56fd748
Chris Bolton <c@chrisbolton.me>
2012-10-03 21:46:16 -0700

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :name, :email, :token, :uid, :password, :password_confirmation
+  attr_accessible :name, :email, :token, :uid, :password
 end
