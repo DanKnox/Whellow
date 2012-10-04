4b37659b120eb782a502cbe0c3a6b48bb13766d0
Chris Bolton <c@chrisbolton.me>
2012-10-03 22:33:30 -0700

    fix

--- db/migrate/20120930162403_add_info_to_users.rb
+++ db/migrate/20120930162403_add_info_to_users.rb
@@ -1,6 +1,6 @@
 class AddInfoToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :token, :string, :limit => 300
+    add_column :users, :token, :text, :limit => nil
     add_column :users, :uid, :string
     add_column :users, :name, :string
   end
