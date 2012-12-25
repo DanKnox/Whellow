7b9646dbb79662751271cea74b9fe8d44389783f
Chris Bolton <c@chrisbolton.me>
2012-12-24 16:39:30 -0800

    moved api fetching to user model

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -9,26 +9,7 @@ class SiteController < ApplicationController
 
   def posts
     since = (Time.now.to_i - 3600) * 1000
-
-    fb = Koala::Facebook::API.new current_user.facebook_token
-    #fb_posts = fb.get_connections('me', 'feed')
-
-    #twitter = Twitter::Client.new(
-    #  oauth_token: current_user.twitter_token,
-    #  oauth_token_secret: current_user.twitter_secret
-    #)
-    
-    t_url = 'https://api.twitter.com/1/statuses/home_timeline.json'
-    param_string = "oauth_consumer_key=#{ ENV['TWITTER_KEY'] }&oauth_nonce=#{ rand(36**40).to_s(36) }&oauth_signature_method=HMAC-SHA1&oauth_timestamp=#{ Time.now.to_i }&oauth_token=#{ CGI.escape current_user.twitter_token }&oauth_version=1.0"
-    sig_string = "GET&#{ CGI.escape t_url }&#{ CGI.escape param_string }"
-    sig_key = ENV['TWITTER_SECRET'] + '&' + current_user.twitter_secret
-    hmac = HMAC::SHA1.new sig_key
-    hmac.update sig_string
-    twitter_posts = HTTParty.get('https://api.twitter.com/1/statuses/home_timeline.json', headers: { 'Authorization' => "OAuth #{ param_string.split('&').join(', ') }, oauth_signature=#{ CGI.escape( Base64.encode64(hmac.digest)[0..-2] )}" })
-
     binding.pry
-    twitter_posts = twitter.home_timeline
-    #linkedin = HTTParty.get('https://api.singly.com/v0/services/linkedin/network', { query: { limit: 50, since: since, access_token: cookies[:access_token] } }).parsed_response
     posts = fb + twitter
     render json: posts.sort_by{|p|p[:at]}
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   attr_accessible :name, :email, :token, :uid, :password
+
+  def facebook
+    Koala::Facebook::API.new(self.facebook_token).get_connections('me', 'feed')
+  end
+
+  def twitter
+    t_url = 'https://api.twitter.com/1/statuses/home_timeline.json'
+    param_string = "oauth_consumer_key=#{ ENV['TWITTER_KEY'] }&oauth_nonce=#{ rand(36**40).to_s(36) }&oauth_signature_method=HMAC-SHA1&oauth_timestamp=#{ Time.now.to_i }&oauth_token=#{ CGI.escape self.twitter_token }&oauth_version=1.0"
+    sig_string = "GET&#{ CGI.escape t_url }&#{ CGI.escape param_string }"
+    sig_key = ENV['TWITTER_SECRET'] + '&' + self.twitter_secret
+    hmac = HMAC::SHA1.new sig_key
+    hmac.update sig_string
+    HTTParty.get('https://api.twitter.com/1/statuses/home_timeline.json', headers: { 'Authorization' => "OAuth #{ param_string.split('&').join(', ') }, oauth_signature=#{ CGI.escape( Base64.encode64(hmac.digest)[0..-2] )}" })
+  end
 end
